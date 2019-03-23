c32db7e0cc1e625dfe9c4deb309bd478cd3a4b99
unknown <devethancraig@gmail.com>
2019-03-22 19:21:47 -0600

    Preped for Heroku

--- Server.js
+++ Server.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const projects = require('./routes/api/Projects');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const db = require('./config/Keys').mongoURI;
-
-mongoose.connect(db)
-    .then(() => console.log('MongoDB Connected.'))
-    .catch(err => console.log(err));
-
-app.use('/api/projects', projects)
-
-const Port = process.env.PORT || 5100;
+const Port = process.env.PORT || 5000;
 
 app.listen(Port, () => console.log(`Server is running on port ${Port}.`));
