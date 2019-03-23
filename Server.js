4a10004e93631cb50714b17bea954e240235d063
unknown <devethancraig@gmail.com>
2019-03-23 16:29:00 -0600

    Added path

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 require("dotenv").config({
     path: path.join(__dirname, "./.env")
