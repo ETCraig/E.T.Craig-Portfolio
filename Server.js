e5a398b0b128e14d356441c67f4efbbe6d0950c2
unknown <devethancraig@gmail.com>
2019-03-23 16:21:33 -0600

    Added dotenv & call

--- Server.js
+++ Server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+require("dotenv").config({
+    path: path.join(__dirname, "./.env")
+});
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const Port = process.env.PORT || 5000;
 
